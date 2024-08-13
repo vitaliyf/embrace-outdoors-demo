d8af2518cf0ee5d941e7b53295faa4fe6e8a8fe9
davidlawrencer <davidlrifkin+github@gmail.com>
2024-08-13 05:48:47 -0500

    iOS: Add Mock Timing

---
+++ iOS/embrace-outdoors-ios/embrace-outdoors-ios/Utils/TimeInterval+MockTiming.swift
@@ -0,0 +1,24 @@
+//
+//  TimeInterval+MockTiming.swift
+//  embrace-outdoors-ios
+//
+//  Created by David Rifkin on 8/13/24.
+//
+
+import Foundation
+
+// random numbers for variable trace timing. returns millisecond intervals
+extension TimeInterval {
+        
+    static var mockShortTimeInterval: Double {
+        Double.random(in: 0.001...0.1)
+    }
+    
+    static var mockMediumTimeInterval: Double {
+        Double.random(in: 0.1...0.25)
+    }
+    
+    static var mockLongTimeInterval: Double {
+        Double.random(in: 0.26...1.00)
+    }
+}
